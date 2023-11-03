16949a1a917f9563cd06177b2d5cb5391c38528f
milton <miltonelim64@gmail.com>
2023-11-03 12:46:35 +0300

    still trying out react native testing library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
+// jest.config.js
+
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest', // Transform JavaScript files using Babel
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+  ],
 };
